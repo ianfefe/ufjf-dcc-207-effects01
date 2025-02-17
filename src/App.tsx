095be6dd32c2f5cee06750d10bf4fbddd0e3f107
ianfefe <felix.ian@estudante.ufjf.br>
2025-02-17 20:14:50 -0300

    add prompt

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
+import Prompt from './Prompt';
 
 function App() {
   const [estadoContador, setEstadoContador] = useState(0);
+  let variavelContador = 0;
+  const refContador = useRef(0);
 
   return (
     <>
-      <div className="card">
-        <button onClick={() => setEstadoContador((count) => count + 1)}>
+    <div>
+      <Prompt/>
+    </div>
+
+      <div>
+        <button onClick={() => {
+          setEstadoContador((count) => count + 1);
+          console.log("estadoContador", estadoContador);
+        }}>
           estadoContado {estadoContador}
+        </button>
+        <button onClick={() => {
+          variavelContador += 1;
+          console.log("variavelContador", variavelContador);
+        }}>
+          variavelContador {variavelContador}(!)
+        </button>
+        <button onClick={() => {
+          refContador.current += 1;
+          console.log("refContador", refContador.current);
+        }}>
+          refContador {refContador.current}(!)
         </button>
       </div>
     </>
---
+++ src/Prompt.tsx
@@ -0,0 +1,25 @@
+import React, { useRef, useState } from "react";
+
+export default function Prompt(){
+    const [entrada, setEntrada] = useState("");
+    const [saida, setSaida] = useState("");
+    const refInput = useRef<HTMLInputElement | null>(null);
+
+    function onEntradaChange(e:React.ChangeEvent<HTMLInputElement>){
+        setEntrada(e.target.value)
+    }
+
+    function onExecutarClique(){
+        setSaida(entrada);
+        if(!refInput.current) return;
+        refInput.current.select();
+    }
+
+    return (
+        <div>
+            <input ref={refInput} value={entrada} onChange={onEntradaChange} />
+            <button onClick={onExecutarClique}>Executar</button>
+            <p>{saida}</p>
+        </div>
+    );
+}
